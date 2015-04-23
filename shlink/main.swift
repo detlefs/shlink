d1de1cbf9864b26869e3a5ccaf8c86c7ea54f68c
Detlef Schneider <detlefs@gmail.com>
2015-04-23 21:52:06 +0200

    Initial version

--- shlink/main.swift
+++ shlink/main.swift
@@ -8,5 +8,40 @@
 
 import Foundation
 
-println("Hello, World!")
+let myProcess = NSProcessInfo()
 
+if (myProcess.arguments.count < 2)
+{
+	println("Usage 1:\t\(myProcess.processName) [source] [target]")
+	println("\t\thard-links the source directory to the target.\n")
+	println("Usage 2:\t\(myProcess.processName) [-u] [target]")
+	println("\t\tunlinks the target directory.")
+	exit (1)
+}
+else
+{
+	let arg1: String = myProcess.arguments[1] as! String
+	var ret : Int32
+	
+	if (arg1 == "-u")
+	{
+		let arg_trg: String = myProcess.arguments[2] as! String
+		
+		println("Unlinking \(arg_trg).")
+		ret = unlink(arg_trg)
+	}
+	else
+	{
+		let arg_src: String = myProcess.arguments[1] as! String
+		let arg_trg: String = myProcess.arguments[2] as! String
+		
+		println("Linking \(arg_src) to \(arg_trg).")
+		ret = link(arg_src, arg_trg)
+	}
+	
+	if (ret != 0)
+	{
+		perror(myProcess.processName)
+	}
+	exit(ret)
+}
