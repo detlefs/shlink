f668ebbfde4afc2755ab1b19bd769b787a08fddf
Detlef Schneider <detlefs@gmail.com>
2015-04-23 20:03:30 +0200

    Initial Commit

---
+++ shlink/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  shlink
+//
+//  Created by Detlef Schneider on 23.04.15.
+//  Copyright (c) 2015 Schneider-Zone. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
