8893941fed8a8eed9b1ff2c9b14993aa9da43b37
detlefs <detlefs@me.com>
2016-03-19 11:49:54 +0100

    Update

--- shlink/main.swift
+++ shlink/main.swift
@@ -3,7 +3,7 @@
 //  shlink
 //
 //  Created by Detlef Schneider on 23.04.15.
-//  Copyright (c) 2015 Schneider-Zone. All rights reserved.
+//  Copyright (c) 2015-2016 Schneider-Zone. All rights reserved.
 //
 // Tool to create hardlinks of folders
 
@@ -15,33 +15,33 @@ let myProcess = NSProcessInfo()
 if (myProcess.arguments.count < 2)
 {
 	// Print syntax information, if not enough command line arguments
-	println("Usage 1:\t\(myProcess.processName) [source] [target]")
-	println("\t\thard-links the source directory to the target.\n")
-	println("Usage 2:\t\(myProcess.processName) [-u] [target]")
-	println("\t\tunlinks the target directory.")
+	print("Usage 1:\t\(myProcess.processName) [source] [target]")
+	print("\t\thard-links the source directory to the target.\n")
+	print("Usage 2:\t\(myProcess.processName) [-u] [target]")
+	print("\t\tunlinks the target directory.")
 	exit (1)
 }
 else
 {
 	// Parse command line arguments
-	let arg1: String = myProcess.arguments[1] as! String
+	let arg1: String = myProcess.arguments[1]
 	var ret : Int32
 	
 	if (arg1 == "-u")
 	{
 		// Unlink option
-		let arg_trg: String = myProcess.arguments[2] as! String
+		let arg_trg: String = myProcess.arguments[2] 
 		
-		println("Unlinking \(arg_trg).")
+		print("Unlinking \(arg_trg).")
 		ret = unlink(arg_trg)
 	}
 	else
 	{
 		// Source and target given
-		let arg_src: String = myProcess.arguments[1] as! String
-		let arg_trg: String = myProcess.arguments[2] as! String
+		let arg_src: String = myProcess.arguments[1] 
+		let arg_trg: String = myProcess.arguments[2] 
 		
-		println("Linking \(arg_src) to \(arg_trg).")
+		print("Linking \(arg_src) to \(arg_trg).")
 		ret = link(arg_src, arg_trg)
 	}
 	
